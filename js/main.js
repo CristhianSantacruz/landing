f95de4eb9702866e123994a33ece71a1333c3935
CristhianSantacruz <116905281+cecilitaortiz@users.noreply.github.com>
2024-06-03 10:56:56 -0500

    main js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let loaded = (eventLoaded) => {
+    window.alert("Bienvenido a landing page loaded")
+    let myform = document.getElementById("contactform")
+    myform.addEventListener('submit', (eventSubmit ) => { 
+
+        debugger;
+      })
+    console.log(eventLoaded)
+    debugger
+}
+
+window.addEventListener("DOMContentLoaded",loaded)
