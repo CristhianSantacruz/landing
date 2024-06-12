33662728ad9f022a450aaa5925420a279e944c3d
Cristhian Santacruz <crimsant@espol.edu.ec>
2024-06-11 21:29:56 -0500

    contact post/get

--- js/info-users.js
+++ js/info-users.js
@@ -4,9 +4,32 @@
 
   console.log("Datos:", data);
 
-  const tableBody = document.getElementById("tablebody");
-  tableBody.innerHTML = ''; // Limpiar cualquier contenido existente en la tabla
+  const container = document.getElementById("tarjetas-container");
+  container.innerHTML = ''; 
+  const resumenContainer = document.getElementById("resumen-container")
 
+  const emailCounts = {};
+  Object.keys(data).forEach(key => {
+    const email = data[key].email;
+    if (email) { 
+      if (emailCounts[email]) {
+        emailCounts[email]++;
+      } else {
+        emailCounts[email] = 1;
+      }
+    }
+  });
+
+
+ // Generar el resumen
+ let resumenHTML = '';
+ for (const email in emailCounts) {
+    resumenHTML += `<p> <span> ${email} </span>: ${emailCounts[email]} solicitudes</p>`;
+   
+ }
+ resumenContainer.innerHTML = resumenHTML;
+  
+  
   for (const key in data) {
       if (data.hasOwnProperty(key)) {
           const item = data[key];
@@ -14,16 +37,22 @@
           const email = item.email;
           const phone = item.phone;
           const message = item.mensaje || '';
-          const newRow = document.createElement('tr');
-          newRow.innerHTML = `
-              <td>${nombre}</td>
-              <td>${email}</td>
-              <td>${phone}</td>
-              <td>${message}</td>
+          const category = item.categorySelect;
+          
+          container.innerHTML += `
+            <div class="card m-2" style="width: 18rem;">
+              <img class="card-img-top" src="./images/my-images/klashzend-1.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title" id="h5email">${email}</h5>
+                <p class="card-text">${message}</p>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Nombre: ${nombre}</li>
+                  <li class="list-group-item">Teléfono: ${phone}</li>
+                  <li class="list-group-item">Categoría: ${category}</li>
+                </ul>
+              </div>
+            </div>
           `;
-
-          // Agregar la nueva fila al tbody
-          tableBody.appendChild(newRow);
       }
   }
 })()
--- js/savemessage.js
+++ js/savemessage.js
@@ -1,18 +1,51 @@
+
+
 const formulario = document.getElementById("miformulario");
 formulario.addEventListener("submit", (event) => {
   event.preventDefault();
-  const nombre = document.getElementById("fname").value;
-  const apellido = document.getElementById("lname").value;
-  const email = document.getElementById("email").value;
-  const mensaje = document.getElementById("message").value;
-  const phone = document.getElementById("phone").value
+  const nombre = document.getElementById("fname")
+  const apellido = document.getElementById("lname")
+  const email = document.getElementById("email")
+  const mensaje = document.getElementById("message")
+  const phone = document.getElementById("phone")
+  const categorySelect = document.getElementById("category")
+
+ 
   const datos = {
-    nombre: nombre,
-    apellido : apellido,
-    email: email,
-    mensaje: mensaje,
-    phone : phone,
+    nombre: nombre.value,
+    apellido : apellido.value,
+    email: email.value,
+    mensaje: mensaje.value,
+    phone : phone.value,
+    categorySelect : categorySelect.value
   };
+
+
+  if(validarCampos(datos) ===false){
+    return;
+  }
+
+  if(phone.value.length !== 10)  {
+    Swal.fire({
+      icon : "warning",
+      text : "El telefono solo debe tener 10 digitos",
+      showConfirmButton : false,
+      timer : 1000
+   });
+   phone.value = ""
+   return;
+  }
+
+  if(categorySelect.value === "") {
+    Swal.fire({
+      icon : "error",
+      text : "Elige la categoria que perteneces",
+      showConfirmButton : false,
+      timer : 1000
+   });
+    return;
+  }
+  
   fetch("https://proyecto-example-web-default-rtdb.firebaseio.com/collection.json", {
     method: "POST",
     body: JSON.stringify(datos),
@@ -22,13 +55,33 @@ formulario.addEventListener("submit", (event) => {
   })
     .then((respuesta) => respuesta.json())
     .then((datos) => {
-      document.getElementById("fname").value = "";
-      document.getElementById("lname").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("message").value = "";
-      document.getElementById("phone").value = "";
+      nombre.value = "";
+      apellido.value = "";
+      email.value = "";
+      mensaje.value = "";
+      phone.value = "";
+      categorySelect.value = "API"     
+      Swal.fire({
+         icon : "success",
+         text : "Se ha enviado correctamente",
+         showConfirmButton : false,
+         timer : 1000
+      });
      
     })
     .catch((error) => console.error(error));
 });
+
+function validarCampos(values) {
+  for (const key in values) {
+    if (values[key] === "") {
+      Swal.fire({
+        icon: "warning",
+        text: "Por favor, proporcione su " + key
+      });
+      return false; 
+    }
+  }
+  return true; 
+}
 
